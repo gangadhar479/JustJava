4cfe7b042470a6347f274aff4eee149ab9a0b0e1
GANGADHAR KORRAPATI <119003080@sastra.ac.in>
2017-01-21 14:41:44 +0530

    added name

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,13 @@
 /**
  * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
- *
+ * <p>
  * package com.example.android.justjava;
  */
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,47 +24,52 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     private static int quantity;
+    String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        quantity=0;
+        quantity = 0;
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-       String name;
         EditText editText = (EditText) findViewById(R.id.name);
         name = editText.getText().toString();
-        String message=" coffee order \n name: "+name;
-        int price=20;
-        boolean cream,coca;
-        cream= ((CheckBox) findViewById(R.id.cream)).isChecked();
-        coca= ((CheckBox) findViewById(R.id.coca)).isChecked();
-        if(coca){
-            price+=12;
-            message=message+"\n added coca.";
+        String message = " coffee order \n name: " + name;
+        int price = 20;
+        boolean cream, coca;
+        cream = ((CheckBox) findViewById(R.id.cream)).isChecked();
+        coca = ((CheckBox) findViewById(R.id.coca)).isChecked();
+        if (coca) {
+            price += 12;
+            message = message + "\n added coca.";
         }
-        if(cream){
-            price+=15;
-            message=message+"\n added cream.";
+        if (cream) {
+            price += 15;
+            message = message + "\n added cream.";
         }
-        message=message+"\n Total="+(NumberFormat.getCurrencyInstance().format(quantity*price))+"\n Thank You!";
-        displayOrder(message);
+        message = message + "\n Total=" + (NumberFormat.getCurrencyInstance().format(quantity * price)) + "\n Thank You!";
+       /* displayOrder(message);*/
+        email(message);
     }
-    public void incr(View view){
+
+    public void incr(View view) {
         quantity++;
         display(quantity);
     }
-    public void decr(View view){
+
+    public void decr(View view) {
         quantity--;
-        if(quantity<0){
+        if (quantity < 0) {
             quantity++;
         }
         display(quantity);
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -74,8 +81,18 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private  void displayOrder(String message){
+    /*private  void displayOrder(String message){
         TextView order = (TextView) findViewById(R.id.order_sum);
-        order.setText(message);
+        order.setText(message);}*/
+    public  void email(String message){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "engineersbynature@gmail.com" });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "coffee order by "+name);
+        intent.putExtra(Intent.EXTRA_TEXT,message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
+
 }
